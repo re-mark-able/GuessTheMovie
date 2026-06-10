3426fdae3db2e8d317e6bddb190d9a1ca06f013d
re-mark-able <markbmaximus@gmail.com>
2026-06-10 10:32:39 +0800

    preferences and layout

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -5,11 +5,25 @@ namespace GuessTheMovie
 {
     public partial class MainPage : ContentPage
     {
+        bool KidFriendly = Preferences.Default.Get("kid_friendly", false);
 
         public MainPage()
         {
             InitializeComponent();
-            DependencyService.Register<User>();     
+            DependencyService.Register<User>();
+            kidFriendly.IsToggled = KidFriendly;
+        }
+
+        private void OnPageSizeChange(object sender, EventArgs e)
+        {
+            if (Width > Height)
+            {
+                ButtonStack.Orientation = StackOrientation.Horizontal;
+            }
+            else
+            {
+                ButtonStack.Orientation = StackOrientation.Vertical;
+            }
         }
 
 
@@ -37,7 +51,7 @@ namespace GuessTheMovie
             User user = DependencyService.Get<User>();
             user.Movies = true;
             user.KidFriendly = kidFriendly.IsToggled;
-
+            Preferences.Default.Set("kid_friendly", kidFriendly.IsToggled);
             StartGame(user);
           
             
@@ -49,7 +63,7 @@ namespace GuessTheMovie
             User user = DependencyService.Get<User>();
             user.Movies = false;
             user.KidFriendly = kidFriendly.IsToggled;
-
+            Preferences.Default.Set("kid_friendly", kidFriendly.IsToggled);
             StartGame(user);
 
         }
